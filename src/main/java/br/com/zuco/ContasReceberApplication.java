0b2396f3e0724811e7451fa199b5907dc23f1f8c
zucoloto <zucoloto.alessandro@gmail.com>
2025-08-06 13:37:09 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/zuco/ContasReceberApplication.java
@@ -0,0 +1,13 @@
+package br.com.zuco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasReceberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasReceberApplication.class, args);
+	}
+
+}
