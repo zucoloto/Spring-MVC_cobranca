2cd32fa60d5f6b87e6a8eb62042c67f0d84bba1f
zucoloto <zucoloto.alessandro@gmail.com>
2025-08-07 10:53:07 -0300

    Mostrando a mensagem de sucesso e inicio do Thymeleaf

--- src/main/java/br/com/zuco/controllers/TituloController.java
+++ src/main/java/br/com/zuco/controllers/TituloController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/titulo")
@@ -20,8 +21,10 @@ public class TituloController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String salvar(Titulo titulo) {
+    public ModelAndView salvar(Titulo titulo) {
         tituloRepository.save(titulo);
-        return "/cadastro_titulo";
+        ModelAndView mv = new ModelAndView("/cadastro_titulo");
+        mv.addObject("mensagem", "Título salvo!");
+        return mv;
     }
 }
