596d11d657945dd2e4ee103508e19ef35f9bf8cf
zucoloto <zucoloto.alessandro@gmail.com>
2025-08-07 11:11:11 -0300

    Montando o combo dinamicamente com Thymeleaf

--- src/main/java/br/com/zuco/controllers/TituloController.java
+++ src/main/java/br/com/zuco/controllers/TituloController.java
@@ -1,12 +1,17 @@
 package br.com.zuco.controllers;
 
+import br.com.zuco.model.StatusTitulo;
 import br.com.zuco.model.Titulo;
 import br.com.zuco.repository.TituloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/titulo")
@@ -26,5 +31,10 @@ public class TituloController {
         ModelAndView mv = new ModelAndView("/cadastro_titulo");
         mv.addObject("mensagem", "Título salvo!");
         return mv;
+    }
+
+    @ModelAttribute("todosStatusTitulo")
+    public List<StatusTitulo> todosStatusTitulo() {
+        return Arrays.asList(StatusTitulo.values());
     }
 }
--- src/main/java/br/com/zuco/model/StatusTitulo.java
+++ src/main/java/br/com/zuco/model/StatusTitulo.java
@@ -2,6 +2,7 @@ package br.com.zuco.model;
 
 public enum StatusTitulo {
 
+    CANCELADO("Cancelado"),
     PENDENTE("Pendente"),
     RECEBIDO("Recebido");
 
