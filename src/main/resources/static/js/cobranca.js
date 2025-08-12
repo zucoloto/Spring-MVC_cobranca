aab597c6d5516280052fdec77e3b46d23fc0c140
zucoloto <zucoloto.alessandro@gmail.com>
2025-08-12 14:47:16 -0300

    Máscara de entrada de valores

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -16,6 +16,10 @@ $(function() {
 	$('[rel="tooltip"]').tooltip();
 });
 
+$(function() {
+	$('.js-currency').maskMoney({decimal: ',', thousands: '.', allowZero: true});
+});
+
 $(document).ready(function () {
       window.setTimeout(function() {
           $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
