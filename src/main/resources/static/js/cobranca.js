8f04a62bd9846b5426378f108d59bf0f27da4ac5
zucoloto <zucoloto.alessandro@gmail.com>
2025-08-12 11:23:06 -0300

    Implementando a exclusão

---
+++ src/main/resources/static/js/cobranca.js
@@ -0,0 +1,13 @@
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
+	var button = $(event.relatedTarget);
+	var codigoTitulo = button.data('codigo');
+	var modal = $(this);
+    var form = modal.find('form');
+    var action = form.attr('action');
+
+    if (!action.endsWith('/')) {
+		action += '/';
+	}
+
+    form.attr('action', action + codigoTitulo);
+});
